a6238c189cf314fddffd8f72c7f28cc9290a8950
Jorian Roelofsen <J.Roelofsen2@student.han.nl>
2023-04-14 16:27:29 +0200

    add nodes

--- Onderzoeken/JavascriptOnderzoek/js-onderzoek/src/App.jsx
+++ Onderzoeken/JavascriptOnderzoek/js-onderzoek/src/App.jsx
@@ -4,14 +4,12 @@ import 'reactflow/dist/style.css';
 
 import './updatenode.css';
 
-import CustomNode from './CustomNode';
-
 const initialNodes = [
 
-  { id: '1', type: 'customnode', position: { x: 100, y: 100 } },
-  { id: '2', type: 'customnode', position: { x: 100, y: 200 } },
-  { id: '3', type: 'customnode', position: { x: 200, y: 200 } },
-  { id: '4', type: 'customnode', position: { x: 200, y: 100 } },
+  { id: '1', position: { x: 100, y: 100 } },
+  { id: '2', position: { x: 100, y: 200 } },
+  { id: '3', position: { x: 200, y: 200 } },
+  { id: '4', position: { x: 200, y: 100 } },
 ];
 
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
@@ -21,19 +19,27 @@ const UpdateNode = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const nodeTypes = {
-    customnode: CustomNode,
-  };
-
   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  const addNode = (node) => {
+    setNodes((nds) => nds.concat(node));
+  }
+
+  const getNextNodeId = () => {
+    const lastNode = nodes[nodes.length - 1];
+    return lastNode.id + 1;
+  }
 
   return (
       <div className="fullscreen">
         <h1>Update Node</h1>
+        <button onClick={() => addNode({
+          id: getNextNodeId(),
+          position: { x:75, y:75 },
+          data: { label: `Node` },
+        })}>Update Node</button>
         <ReactFlow
             nodes={nodes}
             edges={edges}
-            nodeTypes={nodeTypes}
             onNodesChange={onNodesChange}
             onEdgesChange={onEdgesChange}
             defaultViewport={defaultViewport}
