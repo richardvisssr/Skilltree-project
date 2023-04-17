e0c462633772b71ed16ba9d840ca9c6c10359dca
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-04-17 15:52:13 +0200

    added testcomponent and stylesheet

--- Code/skilltree-webapp/src/screens/testScreen.jsx
+++ Code/skilltree-webapp/src/screens/testScreen.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import TestComponent from '../components/testComponent';
+
 export default function testScreen() {
   return (
-    <h1>Test screen</h1>
+    <div>
+      <h1>Test screen</h1>
+      <TestComponent />
+    </div>
   )
 }
