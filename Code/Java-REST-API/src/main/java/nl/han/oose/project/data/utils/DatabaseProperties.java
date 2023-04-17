a4314c69db8c2d88e39603846010c90726e9ee62
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-04-17 14:24:25 +0200

    Java Rest API
    Java Rest API:
    - 3 layers
    - beans.xml
    - test endpoint uitgewerkt

---
+++ Code/Java-REST-API/src/main/java/nl/han/oose/project/data/utils/DatabaseProperties.java
@@ -0,0 +1,24 @@
+package nl.han.oose.project.data.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DatabaseProperties {
+    private Logger logger = Logger.getLogger(getClass().getName());
+    private Properties properties;
+    public DatabaseProperties() throws Exception {
+        properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream("database.properties"));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Can't access property file database.properties", e);
+        }
+    }
+
+    public String connectionString()
+    {
+        return properties.getProperty("connectionString");
+    }
+}
---
+++ Code/Java-REST-API/src/main/java/nl/han/oose/project/resources/dto/TestApplication.java
@@ -0,0 +1,8 @@
+package nl.han.oose.project.resources.dto;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class TestApplication extends Application {
+}
