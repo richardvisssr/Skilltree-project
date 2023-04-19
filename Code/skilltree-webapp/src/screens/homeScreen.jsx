7895159b2c09cd1b58e36439d4c4781759447769
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-04-19 15:07:16 +0200

    fix

--- Code/skilltree-webapp/src/screens/homeScreen.jsx
+++ Code/skilltree-webapp/src/screens/homeScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SidebarComponent from '../components/sidebarComponent';
 import TopbarComponent from '../components/topbarComponent';
-import ReactFlow from 'reactflow';
+// import ReactFlow from 'reactflow';
 
 const skilltreeSelected = true;
 
@@ -11,7 +11,6 @@ const selectedSkilltree = () => {
         return (
             <div>
             <TopbarComponent />
-            <ReactFlow />
             </div>
         )
     }
@@ -20,7 +19,9 @@ const selectedSkilltree = () => {
 export default function homeScreen() {
     return (
         <div>
-            <SidebarComponent />
+            <div>
+                <SidebarComponent />
+            </div>
             {selectedSkilltree()}
         </div>
     )
