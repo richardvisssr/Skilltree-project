ae4b1923237e47430b32f1bb4b9d027425a2a161
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-04-19 10:47:34 +0200

    Update topbar.jsx

--- Code/skilltree-webapp/src/components/topbar.jsx
+++ Code/skilltree-webapp/src/components/topbar.jsx
@@ -70,18 +70,7 @@ export default function Example() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
                 <button class="border-solid border-green-600 border-2 bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md mx-3 px-5 py-2 text-sm font-medium">Opslaan</button>
-               
-
-                {/* <button
-                  type="button"
-                  className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button> */}
-
               </div>
             </div>
           </div>
