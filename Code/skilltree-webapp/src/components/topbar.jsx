b962f4c97ca857a47d27e4305f44e9bbe06c38d3
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-04-19 10:43:43 +0200

    Topbar

--- Code/skilltree-webapp/src/components/topbar.jsx
+++ Code/skilltree-webapp/src/components/topbar.jsx
@@ -1,13 +1,7 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+//import { Fragment } from 'react'
+import { Disclosure} from '@headlessui/react' // Menu, Transition
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline' // BellIcon
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-]
 
 const forms = [
     { name: 'name', id: 'name', placeholder: 'Naam'},
@@ -15,9 +9,9 @@ const forms = [
 
 ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(' ')
+// }
 
 export default function Example() {
   return (
@@ -38,68 +32,64 @@ export default function Example() {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+                <div className="flex flex-1">
+                  <div className="flex items-center">
+                    <img
+                      className="block h-8 w-auto lg:hidden"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                      alt="Your Company"
+                    />
+                    <img
+                      className="hidden h-8 w-auto lg:block"
+                      src="https://www.stipbike.nl/wp-content/uploads/2020/02/HAN-logo.png"
+                      alt="Your Company"
+                    />
+                  </div>
 
-                
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                      
-                    ))}
-
-
-                    <div className="relative rounded-md shadow-sm">
-                        {forms.map((item) => (
-                        <input
-                            type="text"
-                            name={item.name}
-                            id={item.id} //block w-full 
-                            className="rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            placeholder={item.placeholder}
-                        />
-                        ))}
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      <div className="relative rounded-md shadow-sm">
+                          {forms.map((item) => (
+                          <input
+                              type="text"
+                              name={item.name}
+                              id={item.id} //block w-full 
+                              //className="rounded-md border-0 py-1.5 px-2 bg-gray-900 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              className='bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md mx-3 px-3 py-2 text-sm font-medium'
+                              placeholder={item.placeholder}
+                          />
+                          ))}
+                      </div>
+                          
                     </div>
                   </div>
-                  
                 </div>
-                
+                <div className="flex flex-1 items-center justify-center">
+                  <button class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md mx-3 px-5 py-2 text-sm font-medium">Node Aanmaken</button>
+                  <button class="bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md mx-3 px-5 py-2 text-sm font-medium">Koppelen</button>
+                </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
+
+                <button class="border-solid border-green-600 border-2 bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md mx-3 px-5 py-2 text-sm font-medium">Opslaan</button>
+               
+
+                {/* <button
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 >
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+                </button> */}
 
               </div>
             </div>
           </div>
+          
 
-          <Disclosure.Panel className="sm:hidden">
+          {/* Mobile dropdown menu */}
+
+          {/* <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
@@ -116,7 +106,8 @@ export default function Example() {
                 </Disclosure.Button>
               ))}
             </div>
-          </Disclosure.Panel>
+          </Disclosure.Panel> */}
+
         </>
       )}
     </Disclosure>
