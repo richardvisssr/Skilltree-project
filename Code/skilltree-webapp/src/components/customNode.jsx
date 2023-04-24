8b1ff7ef6049417d1fe6ca4d07f8745cbe3cd7e4
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-04-24 13:10:49 +0200

    Update customNode.jsx

--- Code/skilltree-webapp/src/components/customNode.jsx
+++ Code/skilltree-webapp/src/components/customNode.jsx
@@ -4,9 +4,9 @@ import "../styles/styles.css";
 
 export default function CustomNode() {
     return (
-        <div className="w-20 h-20 rounded-3xl bg-slate-300">
-            <div className="text-center align-middle">
-                <h2>kaas</h2>
+        <div className="flex w-20 h-20 rounded-3xl bg-slate-300">
+            <div className="text-center align-middle m-auto">
+                <h2>Kaas</h2>
             </div>
         </div>
     );
