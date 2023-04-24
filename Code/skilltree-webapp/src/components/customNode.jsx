f11743e5a45a1ecb5766bc1118bb5bcb38f6a201
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-04-24 12:04:35 +0200

    work in progress commit

--- Code/skilltree-webapp/src/components/customNode.jsx
+++ Code/skilltree-webapp/src/components/customNode.jsx
@@ -4,8 +4,10 @@ import "../styles/styles.css";
 
 export default function CustomNode() {
     return (
-        <div className="w-20 h-20 rounded-3xl bg-amber-300">
-            kaas
+        <div className="w-20 h-20 rounded-3xl bg-slate-300">
+            <div className="text-center align-middle">
+                <h2>kaas</h2>
+            </div>
         </div>
     );
 }
