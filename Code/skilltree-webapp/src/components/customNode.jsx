2f6b3960b89e7bd89ae99f7cf64c1c5786ed1668
Kim Cornelissen <K.Cornelissen3@student.han.nl>
2023-04-25 13:23:41 +0200

    lelijke card

--- Code/skilltree-webapp/src/components/customNode.jsx
+++ Code/skilltree-webapp/src/components/customNode.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import { Handle, Position } from "reactflow";
 import { AiFillEdit } from "react-icons/ai";
 import { EditNodeComponent } from "./editNode/EditNodeComponent";
+import { useState } from "react";
 
 import "./nodeStyle.css";
 
 export default function CustomNode({ isConnectable }) {
     const label = "Kaas";
 
+    const [isEditNodeComponentVisible, setIsEditNodeComponentVisible] = useState(false);
+
+    const showEditNodeComponent = () => {
+        setIsEditNodeComponentVisible(true);
+        if(isEditNodeComponentVisible) {
+            setIsEditNodeComponentVisible(false);
+        }
+    };
+
     return (
-        <div> 
-            <div>
-            <EditNodeComponent />
-            </div>
-        <div className="customNode">
-            <div
-                className="customNodeBody bg-amber-300"
-            >
-                <div className="edit-button">
-                    <button type="button" onClick={EditNodeComponent.showCard}>
+        <div>
+        <div>
+            <div className="customNode">
+                <div className="customNodeBody bg-amber-300">
+                    <div className="edit-button">
+                        <button
+                            className="edit-button"
+                            type="button"
+                            onClick={showEditNodeComponent}
+                        >
                         <AiFillEdit/>
                     </button>
                 </div>
@@ -40,6 +50,8 @@ export default function CustomNode({ isConnectable }) {
                 </div>
             </div>
         </div>
+        </div>
+        {isEditNodeComponentVisible && <EditNodeComponent />}
         </div>
     );
 }
--- Code/skilltree-webapp/src/components/editNode/EditNodeComponent.jsx
+++ Code/skilltree-webapp/src/components/editNode/EditNodeComponent.jsx
@@ -8,10 +8,6 @@ export function EditNodeComponent() {
 
     const [editCardShowState, setEditCardShowState] = useState(true);
     
-    const showCard = () => {
-        setEditCardShowState(true);
-        console.log("Hello");
-    };
 
     const hideCard = () => {
         setEditCardShowState(false);
@@ -24,14 +20,14 @@ export function EditNodeComponent() {
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-5/6">
                         <div className="bg-white px-4 pb-4 pt-5">
-                            <p className="text-center">Gebruik een komma om een nieuwe BeoordelingsCriteria toe te voegen</p>
+                            <p className="text-center">Weet u zeker dat u deze node wil verwijderen?</p>
                             <div className="mt-6 flex items-center justify-center gap-x-6">
                                 <button type="button" className="text-m font-semibold leading-6 text-gray-900" onClick={hideCard}>Annuleren</button>
                                 <button
                                     type="submit"
                                     className="rounded-md bg-indigo-600 px-3 py-2 text-m font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
-                                    Opslaan
+                                    Verwijderen
                                 </button>
                             </div>
                         </div>
