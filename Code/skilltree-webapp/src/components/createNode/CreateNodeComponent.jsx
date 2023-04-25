a8dda3e47f4199e761c24fe75c0638a1dda0a768
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-04-25 09:44:14 +0200

    Update CreateNodeComponent.jsx

--- Code/skilltree-webapp/src/components/createNode/CreateNodeComponent.jsx
+++ Code/skilltree-webapp/src/components/createNode/CreateNodeComponent.jsx
@@ -32,21 +32,21 @@ function CreateNodeComponent() {
     const dispatch = useDispatch();
     // const navigate = useNavigate();
 
-    const [show, setShow] = useState(true);
-    const deleteCard = () => {
-        setShow(false);
+    const [cardShowState, setCardShowState] = useState(true);
+    const hideCard = () => {
+        setCardShowState(false);
         // navigate("/home");
     };
 
     const handleSave = () => {
         dispatch(fetchCreateNodeActionAsync(skill, description, assesmentCriteria, learningOutcome));
-        deleteCard();
+        hideCard();
     };
     /* eslint-disable react/jsx-wrap-multilines */
     /* eslint-disable operator-linebreak */
     return (
         <div>
-            {show ?
+            {cardShowState ?
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-5/6">
                         <div className="bg-white px-4 pb-4 pt-5">
@@ -56,7 +56,7 @@ function CreateNodeComponent() {
                             <FormFieldComponent titel="BeoordelingsCriteria" type="text" value={assesmentCriteria.join(",")} onChange={handleAssessmentCriteriaChange} />
                             <p className="text-center">Gebruik een komma om een nieuwe BeoordelingsCriteria toe te voegen</p>
                             <div className="mt-6 flex items-center justify-center gap-x-6">
-                                <button type="button" className="text-m font-semibold leading-6 text-gray-900" onClick={deleteCard}>Annuleren</button>
+                                <button type="button" className="text-m font-semibold leading-6 text-gray-900" onClick={hideCard}>Annuleren</button>
                                 <button
                                     type="submit"
                                     className="rounded-md bg-indigo-600 px-3 py-2 text-m font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
