ef15fe41d60c4148ae563351d59b5b9eb81e5417
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-04-25 09:17:51 +0200

    rondje

--- Code/skilltree-webapp/src/components/customNode.jsx
+++ Code/skilltree-webapp/src/components/customNode.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Handle, Position } from "reactflow";
 
-import "../styles/styles.css";
+import "./nodeStyle.css";
 
-export default function CustomNode() {
+export default function CustomNode({ isConnectable }) {
+    const label = "Kaas";
+
     return (
-        <div className="flex w-20 h-20 rounded-3xl bg-amber-400">
-            <div className="text-center align-middle m-auto">
-                <h2>Kaas</h2>
+        <div className="customNode">
+            <div
+                className="customNodeBody bg-amber-400"
+            >
+                <Handle
+                    className="targetHandle"
+                    style={{ zIndex: 2 }}
+                    position={Position.Right}
+                    type="source"
+                    isConnectable={isConnectable}
+                />
+                <Handle
+                    className="targetHandle"
+                    position={Position.Left}
+                    type="target"
+                    isConnectable={isConnectable}
+                />
+                {label}
             </div>
         </div>
     );
