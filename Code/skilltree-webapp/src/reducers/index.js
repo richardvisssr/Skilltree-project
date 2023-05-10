976cc5db3fc88c3b67a357f7be804bb3c455a65b
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-05-10 10:32:18 +0200

    .

--- Code/skilltree-webapp/src/reducers/index.js
+++ Code/skilltree-webapp/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import skilltreeReducer from '../reducers/skilltreeReducer';
+import nodeReducer from '../reducers/nodeReducer';
 
 const reducers = combineReducers({
   skilltree: skilltreeReducer,
+  node: nodeReducer,
 });
 
 export default reducers;
