7d0451421f0227e47eff83b6ecb7c044f5a906be
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-05-10 15:56:47 +0200

    add

--- Code/skilltree-webapp/src/components/customNode.jsx
+++ Code/skilltree-webapp/src/components/customNode.jsx
@@ -1,57 +1,84 @@
 import React from "react";
 import { Handle, Position } from "reactflow";
-import { AiFillEdit } from "react-icons/ai";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
 import { EditNodeComponent } from "./editNode/EditNodeComponent";
+import { DeleteNodeComponent } from "./deleteNode/DeleteNodeComponent";
 import { useState } from "react";
 
 import "./nodeStyle.css";
 
 export default function CustomNode({ isConnectable }) {
     const label = "Kaas";
+    const nodeId = -1
 
     const [isEditNodeComponentVisible, setIsEditNodeComponentVisible] = useState(false);
+    const [isDeleteNodeComponentVisible, setIsDeleteNodeComponentVisible] = useState(false);
 
     const showEditNodeComponent = () => {
-        setIsEditNodeComponentVisible(true);
-        if(isEditNodeComponentVisible) {
-            setIsEditNodeComponentVisible(false);
-        }
+        setIsEditNodeComponentVisible(!isEditNodeComponentVisible);
     };
+
+    const showDeleteNodeComponent = () => {
+        setIsDeleteNodeComponentVisible(!isDeleteNodeComponentVisible);
+    };
+
+    const [isNodeVisible, setIsNodeVisible] = useState(true);
+
+    const hideNode = () => {
+        setIsNodeVisible(false);
+    }
 
     return (
         <div>
-        <div>
-            <div className="customNode">
-                <div className="customNodeBody bg-amber-300">
-                    <div className="edit-button">
-                        <button
-                            className="edit-button"
-                            type="button"
-                            onClick={showEditNodeComponent}
-                        >
-                        <AiFillEdit/>
-                    </button>
-                </div>
-                <Handle
-                    className="targetHandle"
-                    style={{ zIndex: 2 }}
-                    position={Position.Right}
-                    type="source"
-                    isConnectable={isConnectable}
-                />
-                <Handle
-                    className="targetHandle"
-                    position={Position.Left}
-                    type="target"
-                    isConnectable={isConnectable}
-                />
-                <div className="flex justify-center items-center w-full">
-                    {label}
+            {isNodeVisible ?
+
+            <div>
+                <div className="customNode">
+                    <div className="customNodeBody bg-amber-300">
+                        <div className="flex justify-between">
+                            <div className="node-button-container">
+                                <button
+                                    className="node-button edit-button"
+                                    type="button"
+                                    onClick={showEditNodeComponent}
+                                >
+                                <AiFillEdit/>
+                                </button>
+                            </div>
+                            <div className="node-button-container">
+                                <button
+                                    className="node-button delete-button"
+                                    type="button"
+                                    onClick={showDeleteNodeComponent}
+                                >
+                                <AiFillDelete/>
+                                </button>
+                            </div>
+                        </div>
+                        <Handle
+                            className="targetHandle"
+                            style={{ zIndex: 2 }}
+                            position={Position.Right}
+                            type="source"
+                            isConnectable={isConnectable}
+                        />
+                        <Handle
+                            className="targetHandle"
+                            position={Position.Left}
+                            type="target"
+                            isConnectable={isConnectable}
+                        />
+                        <div className="flex justify-center items-center w-full">
+                            {label}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        </div>
-        {isEditNodeComponentVisible && <EditNodeComponent />}
+            : null}
+
+            {isEditNodeComponentVisible && <EditNodeComponent />}
+            {isDeleteNodeComponentVisible && <DeleteNodeComponent nodeId={nodeId} showDeleteNodeComponent={showDeleteNodeComponent} hideNode={hideNode} />}
+        
         </div>
     );
 }
