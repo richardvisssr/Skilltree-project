6b101cb12fd8845d377ae81a5238a32c19014f48
Thomas Ubbink <THM.Ubbink@student.han.nl>
2023-05-11 11:00:39 +0200

    Update DeleteNodeComponent.jsx

--- Code/skilltree-webapp/src/components/deleteNode/DeleteNodeComponent.jsx
+++ Code/skilltree-webapp/src/components/deleteNode/DeleteNodeComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { fetchDeleteNodeActionAsync } from "../../actions/Node";
+import { fetchDeleteNodeActionAsync } from "../../actions/NodeAction";
 
 import "../../styles/styles.css";
 
