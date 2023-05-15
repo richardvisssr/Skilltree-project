3033aefd65a418a26826190694d4923693172498
Richard Visser <HJ.Visser@student.han.nl>
2023-05-15 14:10:39 +0200

    Testen geschreven voor service en Resource
    Hele applicatie testen voordat er gemerged kan worden

---
+++ Code/Java-REST-API/src/test/java/nl/han/oose/project/business/services/NodeServiceTest.java
@@ -0,0 +1,89 @@
+package nl.han.oose.project.business.services;
+import static org.mockito.Answers.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import nl.han.oose.project.data.dao.NodeDAO;
+import nl.han.oose.project.resources.dto.NodeDTO;
+import nl.han.oose.project.resources.dto.NodeRequestDTO;
+import nl.han.oose.project.resources.dto.NodesDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeServiceTest {
+
+    private NodeService sut;
+    private NodeDAO nodeDAO;
+
+    @BeforeEach
+    void setup() {
+        sut = new NodeService();
+//        nodeDAO = mock(NodeDAO.class);
+        nodeDAO = mock(NodeDAO.class, RETURNS_DEEP_STUBS);
+
+        sut.setNodeDAO(nodeDAO);
+    }
+
+    @Test
+    void testUpdateNode() {
+        try {
+            // Arrange
+            var nodeDTO = new NodeDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var nodeRequestDTO = new NodeRequestDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var expected = new NodesDTO();
+            expected.setNodes(Arrays.asList(nodeDTO)); // Gebruik Arrays.asList om een lijst te maken
+            doReturn(expected).when(nodeDAO).updateNode(nodeRequestDTO, 1);
+
+            // Act
+            var result = sut.updateNode(nodeRequestDTO, 1);
+
+            // Assert
+            org.junit.jupiter.api.Assertions.assertEquals(expected.getNodes(), result.getNodes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+//
+//
+//
+//    @Test
+//    void testGetAllEdges() {
+//        try {
+//            // Arrange
+//            var edgeDTO = new EdgeDTO("test", "test", "test", 5);
+//            var expected = new EdgesDTO();
+//            expected.setEdges(List.of(edgeDTO));
+//
+//            // Act
+//            var result = sut.getAllEdges(1);
+//
+//            // Assert
+//            org.junit.jupiter.api.Assertions.assertEquals(expected.getEdges(), result.getEdges());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    void testDeleteEdge() {
+//        try {
+//            // Arrange
+//            var expected = new EdgesDTO();
+//
+//            sut.createEdge(new EdgeDTO("test", "test", "test", 5), 1);
+//
+//            // Act
+//            sut.deleteEdge("test");
+//
+//            // Assert
+//            org.junit.jupiter.api.Assertions.assertEquals(expected.getEdges(), sut.getAllEdges(1).getEdges());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- Code/Java-REST-API/src/test/java/nl/han/oose/project/resources/NodeResourceTest.java
+++ Code/Java-REST-API/src/test/java/nl/han/oose/project/resources/NodeResourceTest.java
@@ -68,4 +68,21 @@ public class NodeResourceTest {
             throw new RuntimeException(e);
         }
     }
+
+    @Test
+    void updateNode(){
+        try {
+            //Arrange
+            var expected = Response.Status.OK.getStatusCode();
+            when(nodeService.updateNode(nodeRequestDTO, GEBRUIKER_ID)).thenReturn(nodesDTO);
+
+            //Act
+            var result = sut.updateNode(nodeRequestDTO, GEBRUIKER_ID);
+
+            //Assert
+            Assertions.assertEquals(expected, result.getStatus());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
