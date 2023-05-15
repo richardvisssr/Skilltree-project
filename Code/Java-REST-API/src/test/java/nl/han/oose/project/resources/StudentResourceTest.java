02aeadacef51ed43cc99afcf49e6ccf498892e03
sanad19942 <64159286+sanad19942@users.noreply.github.com>
2023-05-15 15:42:47 +0200

    test

---
+++ Code/Java-REST-API/src/test/java/nl/han/oose/project/resources/StudentResourceTest.java
@@ -0,0 +1,103 @@
+package nl.han.oose.project.resources;
+
+import jakarta.ws.rs.core.Response;
+import nl.han.oose.project.business.services.StudentService;
+import nl.han.oose.project.resources.dto.StudentDTO;
+import nl.han.oose.project.resources.dto.StudentsDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+public class StudentResourceTest {
+    private StudentResource sut;
+    private StudentsDTO studentsDTO;
+    private StudentDTO studentDTO;
+    private StudentService studentService;
+    int skilltreeId = 1;
+    @BeforeEach
+    void setup() {
+        sut = new StudentResource();
+        studentsDTO = mock(StudentsDTO.class);
+        studentDTO = mock(StudentDTO.class);
+        studentService = mock(StudentService.class);
+
+        sut.setStudentService(studentService);
+    }
+
+    @Test
+    void getAllStudents() {
+        try {
+            //Arrange
+            var expected = Response.Status.OK.getStatusCode();
+            when(studentService.getAllStudents()).thenReturn(studentsDTO);
+
+            //Act
+            var result = sut.getAllStudents();
+
+            //Assert
+            Assertions.assertEquals(expected, result.getStatus());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void getAllStudentsWithException() {
+        try {
+            // Arrange
+            var expected = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            when(studentService.getAllStudents()).thenThrow(new SQLException());
+
+            // Act
+            var result = sut.getAllStudents();
+
+            // Arrange
+            Assertions.assertEquals(expected, result.getStatus());
+        } catch (SQLException e) {
+            fail();
+        }
+    }
+
+    @Test
+    void getStudentsBySkilltree() {
+        try {
+            //Arrange
+
+            var expected = Response.Status.OK.getStatusCode();
+            when(studentService.getStudentsBySkilltree(skilltreeId)).thenReturn(studentsDTO);
+
+            //Act
+            var result = sut.getStudentsBySkilltree(skilltreeId);
+
+            //Assert
+            Assertions.assertEquals(expected, result.getStatus());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void getStudentsBySkilltreeWithException() {
+        try {
+            // Arrange
+            var expected = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            when(studentService.getStudentsBySkilltree(skilltreeId)).thenThrow(new SQLException());
+
+            // Act
+            var result = sut.getStudentsBySkilltree(skilltreeId);
+
+            // Arrange
+            Assertions.assertEquals(expected, result.getStatus());
+        } catch (SQLException e) {
+            fail();
+        }
+    }
+
+}
