8ca830c64c9f853ba22396c3732e7660c68059f4
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-05-24 16:02:04 +0200

    Tabjes en woord veranderd

--- Code/skilltree-webapp/src/components/LoginComponent.jsx
+++ Code/skilltree-webapp/src/components/LoginComponent.jsx
@@ -27,14 +27,15 @@ function LoginComponent() {
         const accountsList = accounts.map((account) => (
             <option key={account.firstname + account.lastname} value={account.firstname + " " + account.lastname}>{account.firstname + " " + account.lastname}</option>
         ));
+
         return (
             <select
-                    name="Kies een account"
-                    className="block w-full px-4 py-2 placeholder-gray-400 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                >
-                    <option value="" selected disabled>Selecteer een optie</option>
-                    {accountsList}
-                </select>
+                name="Kies een account"
+                className="block w-full px-4 py-2 placeholder-gray-400 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            >
+                <option value="" selected disabled>Selecteer een gebruiker</option>
+                {accountsList}
+            </select>
         );
     };
 
@@ -43,7 +44,7 @@ function LoginComponent() {
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
             <div className="w-5/6 max-w-md">
                 <div className="bg-white rounded-lg shadow-xl p-6">
-                <h2 className="text-2xl font-semibold mb-4">Login</h2>
+                    <h2 className="text-2xl font-semibold mb-4">Login</h2>
                     {accountList()}
                     <div className="mt-6 flex items-center justify-center">
                         <button
