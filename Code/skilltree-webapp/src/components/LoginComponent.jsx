effccc6c8ed92e39c10dc41798d39b0403c54ea1
Kim Cornelissen <K.Cornelissen3@student.han.nl>
2023-05-24 15:42:17 +0200

    fully done now

--- Code/skilltree-webapp/src/components/LoginComponent.jsx
+++ Code/skilltree-webapp/src/components/LoginComponent.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import "../styles/styles.css";
 
 function LoginComponent() {
@@ -31,7 +30,6 @@ function LoginComponent() {
         return (
             <select
                     name="Kies een account"
-                    id="Kies een account"
                     className="block w-full px-4 py-2 placeholder-gray-400 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 >
                     <option value="" selected disabled>Selecteer een optie</option>
@@ -45,6 +43,7 @@ function LoginComponent() {
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
             <div className="w-5/6 max-w-md">
                 <div className="bg-white rounded-lg shadow-xl p-6">
+                <h2 className="text-2xl font-semibold mb-4">Login</h2>
                     {accountList()}
                     <div className="mt-6 flex items-center justify-center">
                         <button
