9f23f777353e2452b56816e1a088c44ce0bc3416
Kim Cornelissen <K.Cornelissen3@student.han.nl>
2023-05-24 13:57:49 +0200

    fix?

--- Code/skilltree-webapp/src/reducers/skillTreeReducer.js
+++ Code/skilltree-webapp/src/reducers/skillTreeReducer.js
@@ -1,1 +1,30 @@
-"hello?"
+const initialState = {
+    skilltrees: [],
+    currentSkilltree: null,
+    newSkilltree: false,
+  };
+  
+  function skillTreeReducer(state = initialState, action) {
+    switch (action.type) {
+      case 'skilltree/setSkilltrees':
+        return {
+          ...state,
+          skilltrees: action.payload
+        }
+      case 'skilltree/setCurrentSkilltree':
+        return {
+          ...state,
+          currentSkilltree: action.payload,
+          newSkilltree: false
+        }
+      case 'skilltree/addSkilltreeTopbar':
+        return {
+          ...state,
+          currentSkilltree: null,
+          newSkilltree: !state.newSkilltree
+        }
+      default:
+        return state;
+    }
+  }
+  export default skillTreeReducer;
