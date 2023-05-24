58266920fc47e66819a667188f7092ca32f4634b
Kim Cornelissen <K.Cornelissen3@student.han.nl>
2023-05-24 15:31:42 +0200

    visuele front-end done

--- Code/skilltree-webapp/src/components/LoginComponent.jsx
+++ Code/skilltree-webapp/src/components/LoginComponent.jsx
@@ -1,23 +1,51 @@
 import React from "react";
-import RegisterFormFieldComponent from "./RegisterFormFieldComponent";
+import { useDispatch, useSelector } from "react-redux";
 import "../styles/styles.css";
 
 function LoginComponent() {
+
+    // hardcoded accounts
+    const accounts = [
+        {
+            firstname: "Jeroen",
+            lastname: "van der Heijden"
+        },
+        {
+            firstname: "Jeroen",
+            lastname: "van der Heijden"
+        },
+        {
+            firstname: "Jeroen",
+            lastname: "van der Heijden"
+        },
+        {
+            firstname: "Jeroen",
+            lastname: "van der Heijden"
+        },
+    ];
+    
+    const accountList = () => {
+        const accountsList = accounts.map((account) => (
+            <option key={account.firstname + account.lastname} value={account.firstname + " " + account.lastname}>{account.firstname + " " + account.lastname}</option>
+        ));
+        return (
+            <select
+                    name="Kies een account"
+                    id="Kies een account"
+                    className="block w-full px-4 py-2 placeholder-gray-400 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                    <option value="" selected disabled>Selecteer een optie</option>
+                    {accountsList}
+                </select>
+        );
+    };
+
+
     return (
         <div className="flex min-h-screen items-center justify-center bg-gray-100">
             <div className="w-5/6 max-w-md">
                 <div className="bg-white rounded-lg shadow-xl p-6">
-                    <RegisterFormFieldComponent
-                        fieldType="dropdown"
-                        title="Kies een account"
-                        value=""
-                        // onChange={handlePasswordChange}
-                        options={[
-                            { value: "option1", label: "Optie 1" },
-                            { value: "option2", label: "Optie 2" },
-                            { value: "option3", label: "Optie 3" }
-                        ]}
-                    />
+                    {accountList()}
                     <div className="mt-6 flex items-center justify-center">
                         <button
                             type="submit"
