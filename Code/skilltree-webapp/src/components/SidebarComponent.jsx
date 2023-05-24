f7d60cc1c64711234f3e72db4f5a726bcd5e61fd
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-05-24 13:23:17 +0200

    button toegevoegd

--- Code/skilltree-webapp/src/components/SidebarComponent.jsx
+++ Code/skilltree-webapp/src/components/SidebarComponent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { MdPersonAddAlt1 } from "react-icons/md";
 
 import "../styles/styles.css";
 import {
@@ -42,7 +43,7 @@ export default function SidebarComponent() {
                     className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
                     onClick={() => handleButtonClick(skilltree.id)}
                 >
-                    <span className="ml-12">
+                    <span className="ml-4">
                         { skilltree.title }
                     </span>
                 </button>
@@ -57,6 +58,21 @@ export default function SidebarComponent() {
         }
     };
 
+    const registerButton = () => {
+        return(
+        <button
+            type="button"
+            className="flex z-50 items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:text-white group"
+        >
+            <div className="flex" >
+                <MdPersonAddAlt1 size={20} /> 
+                <span className="ml-4"> &nbsp; Registreer</span>
+            </div>  
+        </button>
+        )
+
+    };
+
     const newSkilltreeButton = () => {
         if (newSkilltree) {
             return "-";
@@ -66,11 +82,11 @@ export default function SidebarComponent() {
 
     return (
         <aside
-            id="separator-sidebar"
-            className="top-0 left-0 z-40 w-64 h-screen"
-            aria-label="Sidebar"
+        id="separator-sidebar"
+        className="top-0 left-0 z-40 w-64 h-screen"
+        aria-label="Sidebar"
         >
-            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 flex flex-col">
                 <ul className="space-y-2 font-medium">
                     <li>
                         <button
@@ -78,15 +94,21 @@ export default function SidebarComponent() {
                             type="button"
                             className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                         >
-                            <span className="ml-12">Skilltrees</span>
-                            <span className="mb-1 ml-12 text-3xl">{ newSkilltreeButton() }</span>
+                            <span className="ml-4">Skilltrees</span>
+                            <span className="mb-1 ml-12 text-3xl">{newSkilltreeButton()}</span>
                         </button>
                     </li>
                 </ul>
-                <ul className="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                <ul className="pt-4 mt-4 mb-10 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
                     {skilltreeList()}
                 </ul>
+                <div className="absolute bottom-4 bg-gray-50 dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 w-48">
+                    <ul className="pt-4 mt-auto space-y-2 font-medium ">
+                        {registerButton()}
+                    </ul>
+                </div>
             </div>
         </aside>
+
     );
 }
