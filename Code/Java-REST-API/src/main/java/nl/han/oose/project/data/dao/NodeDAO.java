548c4a801afa873b735114e3dcad57ce42167fef
Jorian Roelofsen <J.Roelofsen2@student.han.nl>
2023-05-25 16:03:37 +0200

    taaie zaak

--- Code/Java-REST-API/src/main/java/nl/han/oose/project/data/dao/NodeDAO.java
+++ Code/Java-REST-API/src/main/java/nl/han/oose/project/data/dao/NodeDAO.java
@@ -14,6 +14,8 @@ public class NodeDAO {
     private NodeDatamapper nodeDatamapper;
     private DatabaseProperties databaseProperties;
     private Connection connection;
+    
+    private PreparedStatement stmt;
 
     public NodesDTO getNodesFromSkillTree(int skilltreeId) throws SQLException {
         try {
@@ -67,18 +69,26 @@ public class NodeDAO {
     }
 
     private ResultSet getAssessmentCriteriaQuery(int skilltreeId) throws SQLException {
-        var query = "SELECT\n" +
-                "ac.Description as AcceptationCriteriaDescription, ac.character, ac.NodeID\n" +
-                "FROM Nodes n\n" +
-                "INNER JOIN AssessmentCriteria ac\n" +
-                "ON n.ID = ac.NodeID\n" +
-                "WHERE n.SkillTreeID = ?";
-        var stmt = connection.prepareStatement(query);
-        stmt.setInt(1, skilltreeId);
-        return stmt.executeQuery();
+        try {
+            var query = "SELECT\n" +
+                    "ac.Description as AcceptationCriteriaDescription, ac.character, ac.NodeID\n" +
+                    "FROM Nodes n\n" +
+                    "INNER JOIN AssessmentCriteria ac\n" +
+                    "ON n.ID = ac.NodeID\n" +
+                    "WHERE n.SkillTreeID = ?";
+            stmt = connection.prepareStatement(query);
+            stmt.setInt(1, skilltreeId);
+            return stmt.executeQuery();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     private ResultSet getNodesQuery(int skilltreeId) throws SQLException {
+        try {
         var query = "SELECT \n" +
                 "\tn.ID, n.Skill, n.Description as NodeDescription, n.PositionX, n.PositionY, n.SkillTreeID,\n" +
                 "\tlo.Description as LearningOutcomeDescription \n" +
@@ -86,17 +96,24 @@ public class NodeDAO {
                 "INNER JOIN LearningOutcome lo\n" +
                 "ON n.ID = lo.NodeID\n" +
                 "WHERE n.SkillTreeID = ?\n";
-        var stmt = connection.prepareStatement(query);
+        stmt = connection.prepareStatement(query);
         stmt.setInt(1, skilltreeId);
         return stmt.executeQuery();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     private int getHighestNodeIdQuery() throws SQLException {
+        try {
         var query = "DECLARE @IdentityValue INT;\n" +
                      "SET @IdentityValue = IDENT_CURRENT('Nodes');\n" +
                      "SELECT @IdentityValue AS 'ID'";
 
-        var stmt = connection.prepareStatement(query);
+        stmt = connection.prepareStatement(query);
         var resultSet = stmt.executeQuery();
 
         int nodeId = 0;
@@ -105,118 +122,191 @@ public class NodeDAO {
         }
 
         return nodeId;
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     private int createNodeQuery(NodeRequestDTO nodeDTO, int skilltreeId) throws SQLException {
-        var insertNodeQuery = "INSERT INTO Nodes (Skill, Description, PositionX, PositionY, SkillTreeID)\n" +
-                "VALUES \n" +
-                "\t(?, ?, ?, ?, ?)\n" +
-                "\n" +
-                "SELECT SCOPE_IDENTITY() as id";
-        var stmt = connection.prepareStatement(insertNodeQuery);
-        stmt.setString(1, nodeDTO.getSkill());
-        stmt.setString(2, nodeDTO.getDescription());
-        stmt.setDouble(3, nodeDTO.getPositionX());
-        stmt.setDouble(4, nodeDTO.getPositionY());
-        stmt.setInt(5, skilltreeId);
-        var resultSet = stmt.executeQuery();
+        try {
+            var insertNodeQuery = "INSERT INTO Nodes (Skill, Description, PositionX, PositionY, SkillTreeID)\n" +
+                    "VALUES \n" +
+                    "\t(?, ?, ?, ?, ?)\n" +
+                    "\n" +
+                    "SELECT SCOPE_IDENTITY() as id";
+            stmt = connection.prepareStatement(insertNodeQuery);
+            stmt.setString(1, nodeDTO.getSkill());
+            stmt.setString(2, nodeDTO.getDescription());
+            stmt.setDouble(3, nodeDTO.getPositionX());
+            stmt.setDouble(4, nodeDTO.getPositionY());
+            stmt.setInt(5, skilltreeId);
+            var resultSet = stmt.executeQuery();
 
-        int nodeId = 0;
-        if(resultSet.next()){
-            nodeId = resultSet.getInt("id");
+            int nodeId = 0;
+            if (resultSet.next()) {
+                nodeId = resultSet.getInt("id");
+            }
+
+            return nodeId;
+        } catch (SQLException e) {
+            throw new SQLException(e);
         }
-
-        return nodeId;
+        finally {
+            stmt.close();
+        }
     }
 
     public void deleteNode(int nodeId) throws SQLException {
-        connection = DriverManager.getConnection(databaseProperties.connectionString());
-        deleteNodeQuery(nodeId);
-        connection.close();
+        try {
+            connection = DriverManager.getConnection(databaseProperties.connectionString());
+            deleteNodeQuery(nodeId);
+            connection.close();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        } finally {
+            connection.close();
+        }
     }
 
     private void deleteNodeQuery(int nodeId) throws SQLException {
-        var deleteNodeQuery = "DELETE FROM Nodes WHERE ID = ?";
-        var stmt = connection.prepareStatement(deleteNodeQuery);
-        stmt.setInt(1, nodeId);
-        stmt.execute();
+        try {
+            var deleteNodeQuery = "DELETE FROM Nodes WHERE ID = ?";
+            stmt = connection.prepareStatement(deleteNodeQuery);
+            stmt.setInt(1, nodeId);
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     private void addAssessmentCriteriaQuery(List<String> assessmentCriteriaDTO, int nodeId) throws SQLException {
-        for (String assessmentCriterium : assessmentCriteriaDTO) {
-            var query = "INSERT INTO AssessmentCriteria (Description, NodeID)\n" +
-                    "VALUES (?, ?)";
-            var stmt = connection.prepareStatement(query);
-            stmt.setString(1, assessmentCriterium);
-            stmt.setInt(2, nodeId);
-            stmt.executeUpdate();
+        try {
+            for (String assessmentCriterium : assessmentCriteriaDTO) {
+                var query = "INSERT INTO AssessmentCriteria (Description, NodeID)\n" +
+                        "VALUES (?, ?)";
+                stmt = connection.prepareStatement(query);
+                stmt.setString(1, assessmentCriterium);
+                stmt.setInt(2, nodeId);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
         }
     }
 
     private void addLearningOutcomeQuery(String learningOutcome, int createdNodeId) throws SQLException {
-        var query = "INSERT INTO LearningOutcome (Description, NodeID)\n" +
-                "VALUES (?, ?)";
-        var stmt = connection.prepareStatement(query);
-        stmt.setString(1, learningOutcome);
-        stmt.setInt(2, createdNodeId);
-        stmt.executeUpdate();
+        try {
+            var query = "INSERT INTO LearningOutcome (Description, NodeID)\n" +
+                    "VALUES (?, ?)";
+            stmt = connection.prepareStatement(query);
+            stmt.setString(1, learningOutcome);
+            stmt.setInt(2, createdNodeId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     private void updateLearningOutcomeQuery(String learningOutcome, int nodeId) throws SQLException {
-        var updateQuery = "UPDATE LearningOutcome SET Description = ? WHERE NodeID = ?";
-        var stmt = connection.prepareStatement(updateQuery);
-        stmt.setString(1, learningOutcome);
-        stmt.setInt(2, nodeId);
-        stmt.executeUpdate();
+        try {
+            var updateQuery = "UPDATE LearningOutcome SET Description = ? WHERE NodeID = ?";
+            stmt = connection.prepareStatement(updateQuery);
+            stmt.setString(1, learningOutcome);
+            stmt.setInt(2, nodeId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     private void updateAssessmentCriteriaQuery(List<String> assessmentCriteriaDTO, int nodeId) throws SQLException {
-        // Delete existing assessment criteria for the given nodeId
-        var deleteQuery = "DELETE FROM AssessmentCriteria WHERE NodeID = ?";
-        var deleteStmt = connection.prepareStatement(deleteQuery);
-        deleteStmt.setInt(1, nodeId);
-        deleteStmt.executeUpdate();
+        try {
+            // Delete existing assessment criteria for the given nodeId
+            var deleteQuery = "DELETE FROM AssessmentCriteria WHERE NodeID = ?";
+            stmt = connection.prepareStatement(deleteQuery);
+            stmt.setInt(1, nodeId);
+            stmt.executeUpdate();
 
-        // Insert updated assessment criteria
-        for (String assessmentCriterion : assessmentCriteriaDTO) {
-            var insertQuery = "INSERT INTO AssessmentCriteria (Description, NodeID)\n" +
-                    "VALUES (?, ?)";
-            var insertStmt = connection.prepareStatement(insertQuery);
-            insertStmt.setString(1, assessmentCriterion);
-            insertStmt.setInt(2, nodeId);
-            insertStmt.executeUpdate();
+            // Insert updated assessment criteria
+            for (String assessmentCriterion : assessmentCriteriaDTO) {
+                var insertQuery = "INSERT INTO AssessmentCriteria (Description, NodeID)\n" +
+                        "VALUES (?, ?)";
+                stmt = connection.prepareStatement(insertQuery);
+                stmt.setString(1, assessmentCriterion);
+                stmt.setInt(2, nodeId);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
         }
     }
 
 
     private void updateNodeQuery(NodeRequestDTO nodeDTO, int nodeId) throws SQLException {
-        var updateNodeQuery = "UPDATE Nodes SET Skill = ?, Description = ? WHERE ID = ?";
-        var stmt = connection.prepareStatement(updateNodeQuery);
-        stmt.setString(1, nodeDTO.getSkill());
-        stmt.setString(2, nodeDTO.getDescription());
-        stmt.setInt(3, nodeId);
-        stmt.executeUpdate();
+        try {
+            var updateNodeQuery = "UPDATE Nodes SET Skill = ?, Description = ? WHERE ID = ?";
+            stmt = connection.prepareStatement(updateNodeQuery);
+            stmt.setString(1, nodeDTO.getSkill());
+            stmt.setString(2, nodeDTO.getDescription());
+            stmt.setInt(3, nodeId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     public NodesDTO updateNodesPositions(NodesDTO nodesDTO, int skilltreeId) throws SQLException {
-        connection = DriverManager.getConnection(databaseProperties.connectionString());
-        for (NodeDTO nodeDTO : nodesDTO.getNodes()) {
-            updateNodePositionsQuery(nodeDTO, skilltreeId);
+        try {
+            connection = DriverManager.getConnection(databaseProperties.connectionString());
+            for (NodeDTO nodeDTO : nodesDTO.getNodes()) {
+                updateNodePositionsQuery(nodeDTO, skilltreeId);
+            }
+            return getNodesFromSkillTree(skilltreeId);
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        } finally {
+            connection.close();
         }
-        connection.close();
-        return getNodesFromSkillTree(skilltreeId);
     }
 
     private void updateNodePositionsQuery(NodeDTO nodeDTO, int skilltreeId) throws SQLException {
-        var query = "UPDATE Nodes\n" +
-                "SET PositionX = ?, PositionY = ?\n" +
-                "WHERE ID = ? AND SkillTreeID = ?";
-        var stmt = connection.prepareStatement(query);
-        stmt.setDouble(1, nodeDTO.getPositionX());
-        stmt.setDouble(2, nodeDTO.getPositionY());
-        stmt.setInt(3, nodeDTO.getID());
-        stmt.setInt(4, skilltreeId);
-        stmt.executeUpdate();
+        try {
+            var query = "UPDATE Nodes\n" +
+                    "SET PositionX = ?, PositionY = ?\n" +
+                    "WHERE ID = ? AND SkillTreeID = ?";
+            stmt = connection.prepareStatement(query);
+            stmt.setDouble(1, nodeDTO.getPositionX());
+            stmt.setDouble(2, nodeDTO.getPositionY());
+            stmt.setInt(3, nodeDTO.getID());
+            stmt.setInt(4, skilltreeId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+        finally {
+            stmt.close();
+        }
     }
 
     @Inject
