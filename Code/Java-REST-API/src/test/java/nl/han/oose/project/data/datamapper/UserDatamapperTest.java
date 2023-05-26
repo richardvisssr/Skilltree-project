d3452fdfa3b1ba306785b54b83ee2915c7de7760
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-05-26 12:51:18 +0200

    merge error fix

--- Code/Java-REST-API/src/test/java/nl/han/oose/project/data/datamapper/UserDatamapperTest.java
+++ Code/Java-REST-API/src/test/java/nl/han/oose/project/data/datamapper/UserDatamapperTest.java
@@ -52,9 +52,7 @@ import java.util.List;
             assertEquals(expectedStudents.get(1).getLastname(), actualStudents.getUsers().get(1).getLastname());
             assertEquals(expectedStudents.get(0).getEmail(), actualStudents.getUsers().get((0)).getEmail());
             assertEquals(expectedStudents.get(1).getEmail(), actualStudents.getUsers().get((1)).getEmail());
-            assertEquals(expectedStudents.get(0).getRoleId(), actualStudents.getUsers().get((0)).getRoleId());
-            assertEquals(expectedStudents.get(1).getRoleId(), actualStudents.getUsers().get((1)).getRoleId());
-            }
+        }
 
         @Test
         void mapResultSetToStudentsDTOWithEmptyResultSet() throws SQLException {
