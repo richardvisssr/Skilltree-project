982cc9e1e5116b591c1dc8717dd3c846e73b565a
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-05-26 10:05:03 +0200

    fix??

--- Code/skilltree-webapp/src/screens/users/studentView.jsx
+++ Code/skilltree-webapp/src/screens/users/studentView.jsx
@@ -23,7 +23,7 @@ export default function StudentView() {
                 <ReactFlowComponentStudent />
             );
         }
-        return;
+        return null;
     }
 
     const skilltreeSelected = () => {
@@ -43,7 +43,7 @@ export default function StudentView() {
                 </div>
             );
         }
-        return;
+        return null;
     };
 
     return (
