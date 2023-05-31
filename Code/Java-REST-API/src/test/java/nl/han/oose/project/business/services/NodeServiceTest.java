176cf0a9e1079c5162abe0259060956b8d573e2b
Kim Cornelissen <K.Cornelissen3@student.han.nl>
2023-05-31 10:41:01 +0200

    NodeService 100%

--- Code/Java-REST-API/src/test/java/nl/han/oose/project/business/services/NodeServiceTest.java
+++ Code/Java-REST-API/src/test/java/nl/han/oose/project/business/services/NodeServiceTest.java
@@ -1,16 +1,15 @@
 package nl.han.oose.project.business.services;
 import static org.mockito.Answers.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
 
 import java.util.Arrays;
+import java.util.List;
+
 import nl.han.oose.project.data.dao.NodeDAO;
-import nl.han.oose.project.resources.dto.NodeDTO;
-import nl.han.oose.project.resources.dto.NodeRequestDTO;
-import nl.han.oose.project.resources.dto.NodesDTO;
+import nl.han.oose.project.resources.dto.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 class NodeServiceTest {
 
@@ -20,70 +19,119 @@ class NodeServiceTest {
     @BeforeEach
     void setup() {
         sut = new NodeService();
-//        nodeDAO = mock(NodeDAO.class);
         nodeDAO = mock(NodeDAO.class, RETURNS_DEEP_STUBS);
 
         sut.setNodeDAO(nodeDAO);
     }
 
-//    @Test
-//    void testUpdateNode() {
-//        try {
-//            // Arrange
-//            var nodeDTO = new NodeDTO(1, "test", "test", 1.0, 1.0, 1, "test");
-//            var nodeRequestDTO = new NodeRequestDTO(1, "test", "test", 1.0, 1.0, 1, "test");
-//            var expected = new NodesDTO();
-//            expected.setNodes(Arrays.asList(nodeDTO)); // Gebruik Arrays.asList om een lijst te maken
-//            doReturn(expected).when(nodeDAO).updateNode(nodeRequestDTO, 1);
-//
-//            // Act
-//            var result = sut.updateNode(nodeRequestDTO, 1);
-//
-//            // Assert
-//            org.junit.jupiter.api.Assertions.assertEquals(expected.getNodes(), result.getNodes());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @Test
+    void testUpdateNode() {
+        try {
+            // Arrange
+            var nodeDTO = new NodeDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var nodeRequestDTO = new NodeRequestDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var expected = new NodesDTO();
+            expected.setNodes(Arrays.asList(nodeDTO)); // Gebruik Arrays.asList om een lijst te maken
+            doReturn(expected).when(nodeDAO).updateNode(nodeRequestDTO, 1);
+
+            // Act
+            var result = sut.updateNode(nodeRequestDTO, 1);
+
+            // Assert
+            org.junit.jupiter.api.Assertions.assertEquals(expected.getNodes(), result.getNodes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testGetAllNodes() {
+        try {
+            // Arrange
+            var nodeDTO = new NodeDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var expected = new NodesDTO();
+            expected.setNodes(Arrays.asList(nodeDTO)); // Gebruik Arrays.asList om een lijst te maken
+            doReturn(expected).when(nodeDAO).getNodesFromSkillTree(1);
+
+            // Act
+            var result = sut.getAllNodes(1);
+
+            // Assert
+            org.junit.jupiter.api.Assertions.assertEquals(expected.getNodes(), result.getNodes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testGetHighestNodeId() {
+        try {
+            // Arrange
+            var expected = 1;
+            doReturn(expected).when(nodeDAO).getHighestNodeId();
+
+            // Act
+            var result = sut.getHighestNodeId();
+
+            // Assert
+            org.junit.jupiter.api.Assertions.assertEquals(expected, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testUpdateNodesPositions() {
+        try {
+            // Arrange
+            var nodeDTO = new NodeDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var expected = new NodesDTO();
+            expected.setNodes(Arrays.asList(nodeDTO)); // Gebruik Arrays.asList om een lijst te maken
+            doReturn(expected).when(nodeDAO).updateNodesPositions(expected, 1);
+
+            // Act
+            var result = sut.updateNodesPositions(expected, 1);
+
+            // Assert
+            org.junit.jupiter.api.Assertions.assertEquals(expected.getNodes(), result.getNodes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testDeleteNode() {
+        try {
+            // Arrange
+            sut.createNode(new NodeRequestDTO(1, "test", "test", 1.0, 1.0, 1, "test"), 1);
+
+            // Act
+            sut.deleteNode(1);
+
+            // Assert
+            verify(nodeDAO, times(1)).deleteNode(anyInt());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testCreateNode() {
+        try {
+            // Arrange
+            var nodeDTO = new NodeDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var nodeRequestDTO = new NodeRequestDTO(1, "test", "test", 1.0, 1.0, 1, "test");
+            var expected = new NodesDTO();
+            expected.setNodes(List.of(nodeDTO));
+            when(nodeDAO.createNode(anyObject(), anyInt())).thenReturn(expected);
+
+            // Act
+            var result = sut.createNode(nodeRequestDTO, 1);
+
+            // Assert
+            org.junit.jupiter.api.Assertions.assertEquals(expected.getNodes(), result.getNodes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
-
-//
-//
-//
-//    @Test
-//    void testGetAllEdges() {
-//        try {
-//            // Arrange
-//            var edgeDTO = new EdgeDTO("test", "test", "test", 5);
-//            var expected = new EdgesDTO();
-//            expected.setEdges(List.of(edgeDTO));
-//
-//            // Act
-//            var result = sut.getAllEdges(1);
-//
-//            // Assert
-//            org.junit.jupiter.api.Assertions.assertEquals(expected.getEdges(), result.getEdges());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    void testDeleteEdge() {
-//        try {
-//            // Arrange
-//            var expected = new EdgesDTO();
-//
-//            sut.createEdge(new EdgeDTO("test", "test", "test", 5), 1);
-//
-//            // Act
-//            sut.deleteEdge("test");
-//
-//            // Assert
-//            org.junit.jupiter.api.Assertions.assertEquals(expected.getEdges(), sut.getAllEdges(1).getEdges());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
