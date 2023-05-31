09391636011ea2b5530191f53de9495b7dc497e6
sanad19942 <64159286+sanad19942@users.noreply.github.com>
2023-05-31 11:36:15 +0200

    DTO test

---
+++ Code/Java-REST-API/src/test/java/nl/han/oose/project/resources/dto/FeedbackDTOTest.java
@@ -0,0 +1,39 @@
+package nl.han.oose.project.resources.dto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FeedbackDTOTest {
+
+    @Test
+    void testConstructorAndGetters() {
+        // Arrange
+        int userId = 1;
+        int nodeId = 2;
+        String feedback = "This is a sample feedback";
+
+        // Act
+        FeedbackDTO feedbackDTO = new FeedbackDTO(userId, nodeId, feedback);
+
+        // Assert
+        Assertions.assertEquals(userId, feedbackDTO.getUserId());
+        Assertions.assertEquals(nodeId, feedbackDTO.getNodeId());
+        Assertions.assertEquals(feedback, feedbackDTO.getFeedback());
+    }
+
+    @Test
+    void testSetters() {
+        // Arrange
+        FeedbackDTO feedbackDTO = new FeedbackDTO(1, 2, "Initial feedback");
+
+        // Act
+        feedbackDTO.setUserId(3);
+        feedbackDTO.setNodeId(4);
+        feedbackDTO.setFeedback("Updated feedback");
+
+        // Assert
+        Assertions.assertEquals(3, feedbackDTO.getUserId());
+        Assertions.assertEquals(4, feedbackDTO.getNodeId());
+        Assertions.assertEquals("Updated feedback", feedbackDTO.getFeedback());
+    }
+}
