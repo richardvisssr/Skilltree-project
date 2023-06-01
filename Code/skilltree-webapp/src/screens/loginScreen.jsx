8f53c010c3de1ae021476f3e315d27b7b0ac9d7e
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-06-01 16:03:33 +0200

    bug fixed

--- Code/skilltree-webapp/src/screens/loginScreen.jsx
+++ Code/skilltree-webapp/src/screens/loginScreen.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Navigate } from 'react-router-dom';
 
 import LoginComponent from "../components/LoginComponent";
 
 export default function LoginScreen() {
-    return (
+    const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    
+    if (currentUser === null) {
+        return (
             <LoginComponent />
-    );
+        );
+    } else {
+        return <Navigate to="/home" />;
+    }
 }
--- Code/skilltree-webapp/src/screens/registerScreen.jsx
+++ Code/skilltree-webapp/src/screens/registerScreen.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Navigate } from 'react-router-dom';
 
 import RegisterAccountComponent from "../components/register/RegisterAccountComponent"
 
 export default function RegisterScreen() {
-    return (
-        <RegisterAccountComponent />
-    );
+    const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    const DOCENT_ROLE = 1;
+    const STUDENT_ROLE = 2;
+
+    if (currentUser !== null) {
+        if (currentUser.roleId === DOCENT_ROLE) {
+            return (
+                <RegisterAccountComponent />
+            );
+        } else if (currentUser.roleId === STUDENT_ROLE) {
+            return <Navigate to="/home" />;
+        }
+    } else {
+        return <Navigate to="/login" />;
+    }
 }
