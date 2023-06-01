0efac4ccb600f1ac8528d4d5f1a719c0bed418a3
Mitchel Fleury <mgf.fleury@student.han.nl>
2023-06-01 10:29:52 +0200

    code smells fix front-end

--- Code/skilltree-webapp/src/screens/homeScreen.jsx
+++ Code/skilltree-webapp/src/screens/homeScreen.jsx
@@ -1,36 +1,33 @@
-import React, { useEffect, useState } from "react";
-import DocentView from "./users/docentView";
-import StudentView from "./users/studentView";
+import React from "react";
 import { Navigate } from 'react-router-dom';
 
+import DocentView from "./users/docentView";
+import StudentView from "./users/studentView";
+
 export default function HomeScreen() {
-  const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+    const currentUserRoleId = currentUser.roleId;
+    let view;
 
-  const currentUserRoleId = currentUser.roleId;
-  let view;
+    if (currentUserRoleId !== null) {
+        switch (currentUserRoleId) {
+        case 1:
+            view = <DocentView />;
+            break;
+        case 2:
+            view = <StudentView />;
+            break;
+        default:
+            view = 'Onbekende userID';
+            break;
+        }
 
-  if (currentUserRoleId !== null) {
-
-    switch (currentUserRoleId) {
-      case 1:
-        view = <DocentView />;
-        break;
-      case 2:
-        view = <StudentView />;
-        break;
-      default:
-        view = 'Onbekende userID';
-        break;
+        return (
+            <div>
+                {view}
+            </div>
+        );
+    } else {
+        return <Navigate to="/login" />
     }
-
-    return (
-      <div>
-        {view}
-      </div>
-    );
-  } else {
-    return (
-      <Navigate to="/login" />
-    );
-  }
 }
